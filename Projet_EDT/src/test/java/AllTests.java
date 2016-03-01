457f2517562eee79f791aeb358efc563c6baa040
DUBUIS Michael <michael.dubuis@etu.univ-amu.fr>
2016-03-01 13:37:29 +0100

    Add test for Hasher and Hexa (from my project GNU)

--- Projet_EDT/src/test/java/AllTests.java
+++ Projet_EDT/src/test/java/AllTests.java
@@ -6,6 +6,8 @@ import business.ManagerTest;
 import business.dao.JpaDaoTest;
 import business.model.SessionFactoryTest;
 import business.model.StudentTest;
+import util.HasherTest;
+import util.HexaTest;
 
 /**
  * @author DUBUIS Michael
@@ -16,7 +18,9 @@ import business.model.StudentTest;
 	StudentTest.class,
 	JpaDaoTest.class,
 	SessionFactoryTest.class,
-	ManagerTest.class})
+	ManagerTest.class,
+	HasherTest.class,
+	HexaTest.class})
 public class AllTests {
 
 }
