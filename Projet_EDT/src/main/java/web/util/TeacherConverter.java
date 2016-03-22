040af9eff83e70bcabd1e1fba759127e7e6bd220
ares <ares@git.git>
2016-03-22 13:57:58 +0100

    newSession now display

--- Projet_EDT/src/main/java/web/util/TeacherConverter.java
+++ Projet_EDT/src/main/java/web/util/TeacherConverter.java
@@ -1,14 +1,20 @@
 package web.util;
 
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 
 import business.dao.DaoException;
 import business.manager.Manager;
 import business.model.users.AbstractUser;
 
+@ManagedBean(name="TeacherConverter")
+@SessionScoped
+@FacesConverter(value = "TeacherConverter")
 public class TeacherConverter implements Converter {
 
 	@ManagedProperty(value="#{containerManager.manager}")
@@ -19,7 +25,7 @@ public class TeacherConverter implements Converter {
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
         // Convert the unique String representation of Foo to the actual Foo object.
         try {
-			return manager.managerSessions.find(value);
+			return manager.managerUsers.find(value);
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		} catch (DaoException e) {
